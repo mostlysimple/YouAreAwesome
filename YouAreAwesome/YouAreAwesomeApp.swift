b3177810be41a96847fb1b588786cd81a720248a
MostlySimple <mostlysimple@gmail.com>
2026-03-15 16:16:53 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Johnny Hubbard on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouAreAwesomeTests/YouAreAwesomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeTests.swift
+//  YouAreAwesomeTests
+//
+//  Created by Johnny Hubbard on 3/15/26.
+//
+
+import Testing
+@testable import YouAreAwesome
+
+struct YouAreAwesomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
