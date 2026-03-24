091a3a01c82d6403e407ab52291152c151a8e6a9
MostlySimple <mostlysimple@gmail.com>
2026-03-24 13:54:18 -0500

    commit

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,15 +6,32 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
+    @State private var soundName = ""
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    
+    let numberOfImages = 10
+    let numberOfSounds = 6
 
     var body: some View {
         
         VStack {
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 120)
+                .animation(.easeInOut(duration: 0.15), value: message)
+
             Spacer()
             
             Image(imageName)
@@ -22,28 +39,55 @@ struct ContentView: View {
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .animation(.default, value: imageName)
             
             Spacer()
             
             Button("Show Message") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
+                let messages = ["You are Awesome!",
+                                "You are Great!",
+                                "You are Amazing!",
+                                "You are Fantastic!",
+                                "You are Incredible!",
+                                "You Make Me Smile!",
+                                "When the Genius Bar Needs Help, They Call You!"]
 
-                message = (message == message1 ? message2 : message1)
-//                imageName = (imageName == "image0" ? "image1": "image0")
+                var messageNumber: Int
+                repeat {
+                    messageNumber = Int.random(in: 0...(messages.count-1))
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
-                //TODO: - Update the imageName variable -
-                imageNumber += 1
+
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...(numberOfImages-1))
+                } while imageNumber == lastImageNumber
                 
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
                 imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+                
+                var soundNumber: Int
+                repeat {
+                    soundNumber = Int.random(in: 0...(numberOfSounds-1))
+                } while soundNumber == lastSoundNumber
+                
+                soundName = "sound\(soundNumber)"
+                lastSoundNumber = soundNumber
+                
+//                let soundName = "sound0"
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print("😡 Could not read file named \(soundName).")
+                    return
+                }
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch {
+                    
+                    print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+                }
                 
                 
             }
